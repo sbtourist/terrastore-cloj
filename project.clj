bd488495441a0cf0cd966fd50610b5301cb04795
Sergio Bossa <sergio.bossa@gmail.com>
2010-09-25 14:58:23 +0200

    Updated to clojure 1.2.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject terrastore-cloj
-  "0.2.0"
+  "0.3.0"
   :description "Terrastore Clojure Client"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojars.rayne/clojure-http-client "1.0.1"]
                  [matchure "0.9.1"]]
  )
