3ae9055a54a709cd8163a5abf2824146c7492793
Sergio Bossa <sergio.bossa@gmail.com>
2010-12-17 18:49:17 +0100

    Bumped up project version to 0.4.0 and upgraded async http client to 0.2.1.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject terrastore-cloj
-  "0.3.0"
+  "0.4.0"
   :description "Terrastore Clojure Client"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [http.async.client "0.2.0"]
+                 [http.async.client "0.2.1"]
                  [matchure "0.9.1"]]
  )
